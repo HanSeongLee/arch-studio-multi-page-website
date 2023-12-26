0751fa0d82404c4e273b5df360eb0c3ee108429b
HanSeongLee <jkhanseong@gmail.com>
2023-12-26 12:26:29 +0900

    feat: add colors

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,11 +1,20 @@
 export type Color = keyof typeof COLORS;
 
 export const COLORS = {
-    BLACK: '#000000',
+    VERY_DARK_BLUE: '#1B1D23',
+    DARK_GREY: '#60636D',
+    MEDIUM_GREY: '#7D828F',
+    LIGHT_GREY: '#C8CCD8',
+    VERY_LIGHT_GREY: '#EEEFF4',
+    RED: '#DF5656',
     WHITE: '#FFFFFF',
 };
 
 export const BRAND_COLORS: { [key: string]: Color; } = {
-    PRIMARY: 'BLACK',
-    PRIMARY_HOVER: 'WHITE',
+    PRIMARY: 'VERY_DARK_BLUE',
+    PRIMARY_HOVER: 'DARK_GREY',
+    PRIMARY_ACTIVE: 'LIGHT_GREY',
+    NEUTRAL: 'DARK_GREY',
+    BACKGROUND: 'WHITE',
+    DANGER: 'RED',
 };
