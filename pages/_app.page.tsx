9a6e72463d0bf2c0049a73d347ca91f7f767c0f1
HanSeongLee <jkhanseong@gmail.com>
2023-12-30 18:56:55 +0900

    fix: add Spartan font and correct font naming

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -4,11 +4,17 @@ import SEO from 'config/next-seo.config';
 import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
 import React from 'react';
-import { League_Spartan } from '@next/font/google';
+import { League_Spartan } from 'next/font/google';
+import localFont from 'next/font/local';
 
-const SpartanFont = League_Spartan({
+const LeagueSpartanFont = League_Spartan({
     weight: ['500', '700'],
     subsets: ['latin'],
+});
+
+const SpartanFont = localFont({
+    src: '../public/fonts/Spartan-Bold.ttf',
+    display: 'swap',
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -23,6 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <style jsx global>{`
                 :root {
                   --spartan-font: ${SpartanFont.style.fontFamily};
+                  --league-spartan-font: ${LeagueSpartanFont.style.fontFamily};
                 }
             `}
             </style>
