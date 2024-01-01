27c8e53ae4eb83a75f5bb6e6e5ef1f9a562a3e41
HanSeongLee <jkhanseong@gmail.com>
2024-01-01 19:33:10 +0900

    fix: remove schema in sanity

--- lib/sanity/schema.ts
+++ lib/sanity/schema.ts
@@ -1,9 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity';
 
-import portfolio from 'lib/sanity/schemas/portfolio';
-
 export const schema: { types: SchemaTypeDefinition[]; } = {
-  types: [
-    portfolio,
-  ],
+  types: [],
 };
