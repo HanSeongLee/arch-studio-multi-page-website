e5e0ef240a859415d60c0220c5ccb41964bead8f
HanSeongLee <jkhanseong@gmail.com>
2024-01-11 20:24:18 +0900

    feat: add SocialCard schema

--- lib/sanity/schema.ts
+++ lib/sanity/schema.ts
@@ -1,9 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity';
 
 import portfolio from 'lib/sanity/schemas/portfolio';
+import socialCard from 'lib/sanity/schemas/social-card';
 
 export const schema: { types: SchemaTypeDefinition[]; } = {
   types: [
-    portfolio,
+    portfolio, socialCard,
   ],
 };
---
+++ lib/sanity/schemas/social-card.ts
@@ -0,0 +1,51 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+    name: 'socialCard',
+    title: 'Social Card',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'avatar',
+            title: 'Avatar',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        }),
+        defineField({
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        }),
+        defineField({
+            name: 'affiliation',
+            title: 'Affiliation',
+            type: 'string',
+        }),
+        defineField({
+            name: 'social',
+            title: 'Social',
+            type: 'object',
+            fields: [
+                defineField({
+                    name: 'linkedin',
+                    title: 'Linkedin URL',
+                    type: 'url',
+                }),
+                defineField({
+                    name: 'twitter',
+                    title: 'Twitter URL',
+                    type: 'url',
+                })
+            ]
+        }),
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            subtitle: 'affiliation',
+            media: 'avatar',
+        },
+    }
+});
