308f5db9fd9531a895dcbf0d575fa89dea8c63d2
HanSeongLee <jkhanseong@gmail.com>
2024-01-12 21:14:02 +0900

    feat: add hero section schema

--- lib/sanity/schema.ts
+++ lib/sanity/schema.ts
@@ -2,9 +2,10 @@ import { type SchemaTypeDefinition } from 'sanity';
 
 import portfolio from 'lib/sanity/schemas/portfolio';
 import socialCard from 'lib/sanity/schemas/social-card';
+import heroSection from 'lib/sanity/schemas/sections/hero-section';
 
 export const schema: { types: SchemaTypeDefinition[]; } = {
   types: [
-    portfolio, socialCard,
+    portfolio, socialCard, heroSection,
   ],
 };
---
+++ lib/sanity/schemas/sections/hero-section.ts
@@ -0,0 +1,60 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+    name: 'heroSection',
+    title: 'Hero Section',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'subtitle',
+            title: 'Subtitle',
+            type: 'string',
+        }),
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        }),
+        defineField({
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        }),
+        defineField({
+            name: 'thumbnail',
+            title: 'Thumbnail',
+            type: 'object',
+            fields: [
+                defineField({
+                    name: 'alt',
+                    title: 'Alt',
+                    type: 'string',
+                }),
+                defineField({
+                    name: 'mobile',
+                    title: 'Mobile',
+                    type: 'image',
+                    options: {
+                        hotspot: true,
+                    },
+                }),
+                defineField({
+                    name: 'tablet',
+                    title: 'Tablet',
+                    type: 'image',
+                    options: {
+                        hotspot: true,
+                    },
+                }),
+                defineField({
+                    name: 'desktop',
+                    title: 'Desktop',
+                    type: 'image',
+                    options: {
+                        hotspot: true,
+                    },
+                }),
+            ],
+        }),
+    ],
+});
