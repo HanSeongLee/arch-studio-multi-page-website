932424ebe6181c90278115d4556dd2c6f195d4fa
HanSeongLee <jkhanseong@gmail.com>
2024-01-15 22:46:34 +0900

    feat: add Map component to ContactDetailsSection component

--- components/pages/contact/ContactDetailsSection/index.stories.tsx
+++ components/pages/contact/ContactDetailsSection/index.stories.tsx
@@ -16,18 +16,28 @@ type Story = StoryObj<typeof ContactDetailsSection>;
 
 export const Default: Story = {
     args: {
-        contactDetailsItems: [
+        items: [
             {
                 name: 'Main Office',
-                email: 'archone@mail.com',
+                mail: 'archone@mail.com',
                 address: '1892  Chenoweth Drive TN',
                 phone: '123-456-3451',
+                location: {
+                    _type: 'geopoint',
+                    lat: 35.8017288,
+                    lng: -88.618196,
+                },
             },
             {
                 name: 'Office II',
-                email: 'archtwo@mail.com',
+                mail: 'archtwo@mail.com',
                 address: '3399  Wines Lane TX',
                 phone: '832-123-4321',
+                location: {
+                    _type: 'geopoint',
+                    lat: 33.3328504,
+                    lng: -96.1706446,
+                },
             }
         ]
     },
--- components/pages/contact/ContactDetailsSection/index.tsx
+++ components/pages/contact/ContactDetailsSection/index.tsx
@@ -1,16 +1,37 @@
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, useRef } from 'react';
 import styles from './style.module.scss';
 import cn from 'classnames';
 import Container from 'components/commons/Container';
 import Title from 'components/commons/Typography/Title';
 import ContactDetailsCardContainer from 'containers/ContactDetailsCardContainer';
 import { IContactDetails } from 'types/contact';
+import Map from 'components/commons/Map';
+import * as Leaflet from 'leaflet';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
-    contactDetailsItems: IContactDetails[];
+    title: string;
+    items: IContactDetails[];
 }
 
-const ContactDetailsSection: React.FC<IProps> = ({ contactDetailsItems, className, children, ...props }) => {
+const ContactDetailsSection: React.FC<IProps> = ({
+                                                     title, items, className, children,
+                                                     ...props
+                                                 }) => {
+    const mapRef = useRef<Leaflet.Map>(null);
+    const bounds: Leaflet.LatLngTuple[] = items.map((item) => {
+        const { location } = item;
+        return [location.lat, location.lng];
+    });
+
+    const handleViewOnMapClick = (item: IContactDetails) => {
+        const { location } = item;
+        const map = mapRef.current;
+        if (!map) {
+            return ;
+        }
+        map.flyTo([location.lat, location.lng]);
+    };
+
     return (
         <section className={cn(styles.contactDetailsSection, className)}
                  {...props}
@@ -20,12 +41,43 @@ const ContactDetailsSection: React.FC<IProps> = ({ contactDetailsItems, classNam
                        as={'h2'}
                        size={'medium'}
                 >
-                    {'Contact \nDetails'}
+                    {title}
                 </Title>
                 <ContactDetailsCardContainer className={styles.contactDetailsCardContainer}
-                                             items={contactDetailsItems}
+                                             items={items}
+                                             onViewOnMapClick={handleViewOnMapClick}
                 />
             </Container>
+            <div className={styles.mapContainer}>
+                <Map bounds={bounds}
+                     ref={mapRef}
+                >
+                    {({ TileLayer, Marker, Popup, useMap }) => (
+                        <>
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                            />
+                            {items.map((item, index) => {
+                                const { location } = item;
+                                return (
+                                    <Marker key={index}
+                                            position={[location.lat, location.lng]}
+                                    >
+                                        <Popup>
+                                            <strong>{item.name}</strong>
+                                            <br /><br />
+                                            Mail: {item.mail}<br />
+                                            Address: {item.address}<br />
+                                            Phone: {item.phone}
+                                        </Popup>
+                                    </Marker>
+                                );
+                            })}
+                        </>
+                    )}
+                </Map>
+            </div>
         </section>
     );
 };
