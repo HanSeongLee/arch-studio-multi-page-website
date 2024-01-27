72d55c40913a1d578fccd1e427c85fe4e02707ef
HanSeongLee <jkhanseong@gmail.com>
2024-01-27 16:44:14 +0900

    feat: add render to PortfolioContainer

--- containers/PortfolioContainer/index.tsx
+++ containers/PortfolioContainer/index.tsx
@@ -6,29 +6,31 @@ import { IPortfolio } from 'types/portfolio';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
     items: IPortfolio[];
-    enableCounter?: boolean;
-    transformDescription?: (item: IPortfolio) => string;
+    render?: (item: IPortfolio) => React.ReactNode;
 }
 
 const PortfolioContainer: React.FC<IProps> = ({
-                                                  items, enableCounter, transformDescription = (item) => convertDateToMonthNameYear(new Date(item.date)), className,
-                                                  children, ...props
+                                                  items, render, className, children,
+                                                  ...props
                                               }) => {
+    const defaultRender = (item: IPortfolio) => {
+        const description = convertDateToMonthNameYear(new Date(item.date));
+
+        return (
+            <PortfolioCard key={item._id}
+                           {...item}
+                           description={description}
+            />
+        );
+    };
+
+    const renderFn = render || defaultRender;
+
     return (
         <div className={cn(className)}
              {...props}
         >
-            {items.map((item, _) => {
-                const description = transformDescription(item);
-
-                return (
-                    <PortfolioCard key={item._id}
-                                   {...item}
-                                   description={description}
-                                   enableCounter={enableCounter}
-                    />
-                );
-            })}
+            {items.map(renderFn)}
         </div>
     );
 };
