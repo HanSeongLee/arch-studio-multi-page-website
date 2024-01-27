2a936c3ff19220884fee8f617fb316debd23eb4c
HanSeongLee <jkhanseong@gmail.com>
2024-01-27 16:32:20 +0900

    feat: add transformDescription to PortfolioContainer

--- containers/PortfolioContainer/index.tsx
+++ containers/PortfolioContainer/index.tsx
@@ -7,18 +7,19 @@ import { IPortfolio } from 'types/portfolio';
 interface IProps extends HTMLAttributes<HTMLDivElement> {
     items: IPortfolio[];
     enableCounter?: boolean;
+    transformDescription?: (item: IPortfolio) => string;
 }
 
 const PortfolioContainer: React.FC<IProps> = ({
-                                                  items, enableCounter, className, children,
-                                                  ...props
+                                                  items, enableCounter, transformDescription = (item) => convertDateToMonthNameYear(new Date(item.date)), className,
+                                                  children, ...props
                                               }) => {
     return (
         <div className={cn(className)}
              {...props}
         >
             {items.map((item, _) => {
-                const description = convertDateToMonthNameYear(new Date(item.date))
+                const description = transformDescription(item);
 
                 return (
                     <PortfolioCard key={item._id}
