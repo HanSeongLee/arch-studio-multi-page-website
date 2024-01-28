0c8a7bbaef3bfcb30990d3dfbeb72c279edbd6bc
HanSeongLee <jkhanseong@gmail.com>
2024-01-28 19:00:07 +0900

    feat: add disable to Container component

--- components/commons/Container/index.tsx
+++ components/commons/Container/index.tsx
@@ -3,12 +3,20 @@ import styles from './style.module.scss';
 import cn from 'classnames';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
-
+    disable?: {
+        mobile?: boolean;
+        tablet?: boolean;
+        desktop?: boolean;
+    };
 }
 
-const Container: React.FC<IProps> = ({ className, children, ...props }) => {
+const Container: React.FC<IProps> = ({ disable, className, children, ...props }) => {
     return (
-        <div className={cn(styles.container, className)}
+        <div className={cn(styles.container, {
+            [styles.disableMobile]: disable?.mobile,
+            [styles.disableTablet]: disable?.tablet,
+            [styles.disableDesktop]: disable?.desktop,
+        }, className)}
              {...props}
         >
             {children}
