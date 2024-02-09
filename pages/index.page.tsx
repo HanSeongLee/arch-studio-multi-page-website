c4ca2c9aa4c4b35802055c3ca8b7096409831f15
HanSeongLee <jkhanseong@gmail.com>
2024-02-09 18:13:29 +0900

    feat: update main page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,34 @@
-import type { NextPage } from 'next'
+import { InferGetStaticPropsType } from 'next';
+import Layout from 'components/commons/Layout';
+import React from 'react';
+import { TITLE_TEMPLATE } from 'config/next-seo.config';
+import { NextSeo } from 'next-seo';
+import SectionContainer from 'containers/SectionContainer';
+import { IPage } from 'types/page';
+import { fetchPage } from 'lib/sanity/queries/page';
 
-const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+const Home = ({ pageInfo }: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const { title, sections } = pageInfo;
+
+    return (
+        <>
+            <NextSeo title={title}
+                     titleTemplate={TITLE_TEMPLATE}
+            />
+            <Layout title={title}>
+                <SectionContainer items={sections} />
+            </Layout>
+        </>
+    );
+};
+
+export const getStaticProps = async () => {
+    const pageInfo: IPage = await fetchPage('/');
+    return {
+        props: {
+            pageInfo,
+        },
+    };
+};
 
 export default Home
